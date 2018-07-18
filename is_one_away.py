c2a1c897c9d1dbb709cb7002832a6060cb87bc22
Samson <sammy_tse@msn.com>
2018-07-18 14:41:48 -0700

    Added starter file for one_away. #5

---
+++ is_one_away.py
@@ -0,0 +1,6 @@
+# One away edits can be grouped into 3 categories: add a character, remove a character and 
+# replace a character. Write a method that checks if a string is one or zero edits 
+# away from another
+
+def is_one_away(string_1, string_2):
+    pass
