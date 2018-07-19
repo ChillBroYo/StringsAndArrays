0ffe889c2be33593f5b1e99eb8c1d5765094bd2f
Samson <sammy_tse@msn.com>
2018-07-19 00:25:27 -0700

    Added some fuinctionality. #5

--- is_one_away.py
+++ is_one_away.py
@@ -2,5 +2,16 @@
 # replace a character. Write a method that checks if a string is one or zero edits 
 # away from another
 
+# First Solution: Go through the first string and check if the second string has all the
+# characters as the first except 1 as with one awaya there should be an exact match on
+# all other elements
 def is_one_away(string_1, string_2):
-    pass
+    unique_items = {}
+
+    for char in string_1:
+        if unique_items.get(char) == None:
+            unique_items[char] = 1
+        else:
+            unique_items[char] += 1
+
+        
